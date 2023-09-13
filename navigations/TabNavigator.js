a6df18b624bb8b702fec53bf1b862ba606067364
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-09-13 23:25:27 +0530

    feat - implemented bottom tab

--- navigations/TabNavigator.js
+++ navigations/TabNavigator.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 import TestScreen from "../screens/TestScreen";
+import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -11,17 +12,47 @@ const TabNavigator = () => {
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name="home"
+              size={30}
+              color={focused ? "#a569bd" : "grey"}
+            />
+          ),
+          tabBarActiveTintColor: "#a569bd",
+        }}
       />
       <Tab.Screen
         name="Test"
         component={TestScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome5
+              name="clipboard-list"
+              size={24}
+              color={focused ? "#a569bd" : "grey"}
+            />
+          ),
+          tabBarActiveTintColor: "#a569bd",
+        }}
       />
       <Tab.Screen
         name="Profile"
         component={ProfileScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name="user"
+              size={24}
+              color={focused ? "#a569bd" : "grey"}
+            />
+          ),
+          tabBarActiveTintColor: "#a569bd",
+        }}
       />
     </Tab.Navigator>
   );
